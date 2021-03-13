8bde637a89871d7b678e680376b4ac766e82fd10
anwar03 <anwarazim03@gmail.com>
2021-03-13 20:37:32 +0600

    Updated Navbar, Error page, about page.

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla saepe corrupti sunt modi tenetur vero voluptatum velit accusamus laborum, quod quas deserunt rerum architecto. Sint vel odit eaque perspiciatis dolorum.
+      </p>
+    </section>
   )
 }
 
